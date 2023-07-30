6b097033eaeb4617dab37f3bd27d43835f23f509
KealvihN <git@KealvihN.cn>
2023-07-31 05:23:39 +0800

    快速开始

--- kaelvihn-service/kaelvihn-user-service/src/main/java/com/kaelvihn/UserApplication.java
+++ kaelvihn-service/kaelvihn-user-service/src/main/java/com/kaelvihn/UserApplication.java
@@ -12,9 +12,16 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date : 2023/7/30 6:42
  */
 
+/**
+ * EnableDiscoveryClient 开启注册服务
+ * EnableFeignClients(clients = OrderFeign.class) 开启openfeign调用
+ * 和扫描OrderFeign类 将OrderFeign加入到spring ioc中
+ * 除此之外 还可以使用
+ * @EnableFeignClients(basePackages = "com.feign.kaelvihn.order") 对整个包进行扫描
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(clients = OrderFeign.class)
+@EnableFeignClients(clients = OrderFeign.class,basePackages = "com.feign.kaelvihn.order")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
